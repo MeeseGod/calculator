908900a82ee0d56351f848113927ef29e0e1f5a7
MeeseGod <63038587+MeeseGod@users.noreply.github.com>
2021-09-08 21:44:19 -0400

    Added completed files
    Added completed versions of core html, js, and css files.

--- script.js
+++ script.js
@@ -1,114 +1,221 @@
-// 0 - 9 = 10 buttons
-// Clear button
-// Period button
-// Backspace Button
-// +, -, /, *
-
-// BUTTONS FOR FUNCTIONS
-// CLEAR
-// SCREEN = DISPLAY
-// EQUALS
-// Your calculator should not evaluate more than a single pair of numbers at a time. 
-//If you enter a number then an operator and another number that calculation should be displayed if your next input is an operator. 
-// The result of the calculation should be used as the first number in your new calculation.
-// You should round answers with long decimals so that they don’t overflow the screen.
-// Pressing “clear” should wipe out any existing data.. make sure the user is really starting fresh after pressing “clear”
-// Display a snarky error message if the user tries to divide by 0… don’t let it crash your calculator!
-// Users can get floating point numbers if they do the math required to get one, but they can’t type them in yet. 
-// Add a . button and let users input decimals! Make sure you don’t let them type more than one though: 12.3.56.5. 
-// It is hard to do math on these numbers. (disable the decimal button if there’s already one in the display)
-// EXTRA CREDIT: Add a “backspace” button, so the user can undo if they click the wrong number.
-// EXTRA CREDIT: Add keyboard support!
-let holdingValue = '';
+let holdingValue = '0';
 let passingInput = '';
-let operatorState = 'Default'
-
+let operatorState = 'Default';
+document.getElementById("heldValue").innerHTML = holdingValue;
 
 function checkInput(e){
     passingInput  += e.value;
-    console.log(passingInput );
     document.getElementById("passingValue").innerHTML = passingInput ;
 }
-
 
 function clearScreen(){
-    holdingValue = ''; 
+    holdingValue = '0'; 
     passingInput  = '';
-    document.getElementById("passingValue").innerHTML = passingInput ;
-    document.getElementById("heldValue").innerHTML = holdingValue;
     operatorState = 'Default'
-}
-
-function test(){
-    console.log("Test");
-}
-
-function decimal(){
-    
+    document.getElementById("passingValue").innerHTML = passingInput;
+    document.getElementById("heldValue").innerHTML = holdingValue;
 }
 
 function add(){
-    if(passingInput != '' && holdingValue != ''){
-        operatorState = 'Add';
-        let x = parseInt(passingInput);
-        let y = parseInt(holdingValue);
-        let output = x + y;
-        holdingValue =  output.toString()
-        document.getElementById("heldValue").innerHTML = holdingValue;
-
-        passingInput  = '';
-        document.getElementById("passingValue").innerHTML = passingInput ;
+    if(operatorState != 'Default' && passingInput != ''){
+        equal()
+        operatorState = 'Adding';
+        passingInput = '';
+        document.getElementById("passingValue").innerHTML = passingInput;
     }
-    else if(passingInput != '' && holdingValue === ''){
-        operatorState = 'Add';
-        let x = parseInt(passingInput);
-        let y = 0;
-        let output = x + y;
-        holdingValue =  output.toString()
+    else if(passingInput != ''){
+        operatorState = 'Adding';
+        holdingValue = passingInput;
+        if(holdingValue > 1000000000){
+            holdingValue = parseInt(holdingValue)
+            holdingValue = holdingValue.toExponential(3);
+        }
+        // Checks to see if holdingValue has floating point, then fixes it to 2 decimal points
+        if(holdingValue.includes('.') && !holdingValue.includes('e')){
+            holdingValue = parseFloat(holdingValue).toFixed(2);
+            holdingValue = holdingValue.toString();
+        }
         document.getElementById("heldValue").innerHTML = holdingValue;
-
-        passingInput  = '';
-        document.getElementById("passingValue").innerHTML = passingInput ;
+        passingInput = ''; 
+        document.getElementById("passingValue").innerHTML = passingInput;
+    }
+    else if(passingInput === '' && holdingValue != '0'){
+        operatorState = 'Adding';
     }
 }
 
 function subtract(){
-    
-    if(passingInput != '' && holdingValue != ''){
+    if(operatorState != 'Default' && passingInput != ''){
+        equal()
         operatorState = 'Subtract';
-        let x = parseInt(passingInput);
-        let y = parseInt(holdingValue);
-        let output = y - x;
-        holdingValue =  output.toString()
-        document.getElementById("heldValue").innerHTML = holdingValue;
-
-        passingInput  = '';
-        document.getElementById("passingValue").innerHTML = passingInput ;
+        passingInput = '';
+        document.getElementById("passingValue").innerHTML = passingInput;
     }
-     else if(passingInput != '' && holdingValue === ''){
-         operatorState = 'Subtract';
-         let x = parseInt(passingInput);
-         let y = 0;
-         let output = x - y;
-       holdingValue =  output.toString()
+    else if(passingInput != ''){
+        operatorState = 'Subtract';
+        holdingValue = passingInput;
+        if(holdingValue > 1000000000){
+            holdingValue = parseInt(holdingValue)
+            holdingValue = holdingValue.toExponential(3);
+        }
+        // Checks to see if holdingValue has floating point, then fixes it to 2 decimal points
+        if(holdingValue.includes('.') && !holdingValue.includes('e')){
+            holdingValue = parseFloat(holdingValue).toFixed(2);
+            holdingValue = holdingValue.toString();
+        }
         document.getElementById("heldValue").innerHTML = holdingValue;
-
-         passingInput  = '';
-         document.getElementById("passingValue").innerHTML = passingInput ;
-     }
+        passingInput = '';
+        document.getElementById("passingValue").innerHTML = passingInput;
+    }
+    else if(passingInput === '' && holdingValue != '0'){
+        operatorState = 'Subtract';
+    }
 }
 
+function multiply(){
+    if(operatorState != 'Default' && passingInput != ''){
+        equal()
+        operatorState = 'Multiply';
+        passingInput = '';
+        document.getElementById("passingValue").innerHTML = passingInput;
+    }
+    else if(passingInput != ''){
+        operatorState = 'Multiply';
+        holdingValue = passingInput;
+        if(holdingValue > 1000000000){
+            holdingValue = parseInt(holdingValue)
+            holdingValue = holdingValue.toExponential(3);
+        }
+        // Checks to see if holdingValue has floating point, then fixes it to 2 decimal points
+        if(holdingValue.includes('.') && !holdingValue.includes('e')){
+            holdingValue = parseFloat(holdingValue).toFixed(2);
+            holdingValue = holdingValue.toString();
+        }
+        document.getElementById("heldValue").innerHTML = holdingValue;
+        passingInput = '';
+        document.getElementById("passingValue").innerHTML = passingInput;
+    }
+    else if(passingInput === '' && holdingValue != '0'){
+        operatorState = 'Multiply';
+    }
+}
+
+function divide(){
+    if(operatorState != 'Default' && passingInput != ''){
+        equal()
+        operatorState = 'Divide';
+        passingInput = '';
+        document.getElementById("passingValue").innerHTML = passingInput;
+    }
+    else if(passingInput != ''){
+        operatorState = 'Divide';
+        holdingValue = passingInput;
+            if(holdingValue > 1000000000){
+                holdingValue = parseInt(holdingValue)
+                holdingValue = holdingValue.toExponential(3);
+            }
+            // Checks to see if holdingValue has floating point, then fixes it to 2 decimal points
+            if(holdingValue.includes('.') && !holdingValue.includes('e')){
+                holdingValue = parseFloat(holdingValue).toFixed(2);
+                holdingValue = holdingValue.toString();
+            }
+        document.getElementById("heldValue").innerHTML = holdingValue;
+        passingInput = '';
+        document.getElementById("passingValue").innerHTML = passingInput;
+        
+    }
+    else if(passingInput === '' && holdingValue != '0'){
+        operatorState = 'Divide';
+    }
+}
+
+function removeLastNumber(){
+    passingInput = passingInput.slice(0, -1);
+    document.getElementById("passingValue").innerHTML = passingInput;
+}
+
+function addDecimalPoint(){
+    if(!passingInput.includes('.') && passingInput != ''){
+        passingInput = passingInput + '.';
+        document.getElementById("passingValue").innerHTML = passingInput;
+    }
+}
+
+// Called to resolve the meeting of the passing value and the held value whenever any operator is chosen
 function equal(){
-    if(operatorState === 'Add'){
-        add();
-        operatorState = 'Default';
+if(passingInput != ''){    
+    if(operatorState === 'Adding'){
+        let x = 0;
+            if(passingInput.includes('.') || holdingValue.includes('.')){
+                x = parseFloat(passingInput) + parseFloat(holdingValue);
+                x = x.toFixed(2);
+            }
+            else{
+                x = parseInt(passingInput) + parseInt(holdingValue);
+            }
+        holdingValue = x.toString();
+        document.getElementById("heldValue").innerHTML = holdingValue;
     }
     else if(operatorState === 'Subtract'){
-        subtract();
-        operatorState = 'Default';
+        let x = 0;
+            if(passingInput.includes('.') || holdingValue.includes('.')){
+                x = parseFloat(holdingValue) - parseFloat(passingInput);
+            }
+            else{
+                x = parseInt(holdingValue) - parseInt(passingInput);
+            }
+        holdingValue = x.toString();
+        document.getElementById("heldValue").innerHTML = holdingValue;
+    }
+    else if(operatorState === 'Multiply'){
+        let x = 0;
+            if(passingInput.includes('.') || holdingValue.includes('.')){
+                x = parseFloat(holdingValue) * parseFloat(passingInput);
+            }
+            else{
+                x = parseInt(holdingValue) * parseInt(passingInput);
+            }
+        holdingValue = x.toString();
+        document.getElementById("heldValue").innerHTML = holdingValue;
+    }
+    else if(operatorState === 'Divide'){
+        if (passingInput === '0'){
+            clearScreen();
+            alert("Dividing by 0? That's a terrible thing to do - Clearing input");
+        }
+        else{
+            let x = 0;
+                if(passingInput.includes('.') || holdingValue.includes('.')){
+                    x = parseFloat(holdingValue) / parseFloat(passingInput);
+                }
+                else{
+                    x = parseInt(holdingValue) / parseInt(passingInput);
+                }
+            holdingValue = x.toString();
+            document.getElementById("heldValue").innerHTML = holdingValue;
+        }
+    }
+    else if(operatorState === 'Default'){
+        holdingValue = passingInput;
+        passingInput = '';
+            // Checks to see if holdingValue has floating point, then fixes it to 2 decimal points
+            if(holdingValue.includes('.')){
+                holdingValue = parseFloat(holdingValue).toFixed(2);
+                holdingValue = holdingValue.toString();
+            }
+        document.getElementById("heldValue").innerHTML = holdingValue;
+        document.getElementById("passingValue").innerHTML = passingInput;
+    }
+    // converts holding value to scientific e notation if value is above 1000000000
+    if(holdingValue > 1000000000){
+        holdingValue = parseInt(holdingValue)
+        holdingValue = holdingValue.toExponential(3);
+        document.getElementById("heldValue").innerHTML = holdingValue;
     }
 }
-
-function operatorChange(){
-    
+    operatorState = 'Default';
+    passingInput = '';
+    document.getElementById("passingValue").innerHTML = passingInput;
 }
+
+console.log("Made by MeeseGod for The Odin Project 🦌 (https://github.com/MeeseGod)")
